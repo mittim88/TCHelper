f986163a0230cb2b07994e6fad54f6a13e94174e
mittim88 <timeschert@yahoo.de>
2024-01-21 08:58:13 +0100

    test @provides

--- TC Helper/TCHelper.lua
+++ TC Helper/TCHelper.lua
@@ -1,6 +1,7 @@
 -- @description TCHelper
 -- @version 2.5.1
 -- @author mittim88
+-- @provides TC Helper/osc.lua
 local version = '2.5.1'
 local testcmd = 'Echo --CONNECTION IS FINE--'
 local script_title = 'TC HELPER'
