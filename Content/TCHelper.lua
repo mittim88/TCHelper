21fdbecf1ffebcf40f2d7158f72ce49fb644a763
mittim88 <timeschert@yahoo.de>
2024-01-21 08:24:07 +0100

    test

--- Content/TCHelper.lua
+++ Content/TCHelper.lua
@@ -1,8 +1,7 @@
 -- @description TCHelper
 -- @version 2.5.1
 -- @author mittim88
--- @provides 
----        /Content/*.lua
+-- @provides /Content/*.lua
 local version = '2.5.1'
 local testcmd = 'Echo --CONNECTION IS FINE--'
 local script_title = 'TC HELPER'
